fea432041e2bcbd10fe16e39a6f8a396bfe9fc6e
AdrianTM <adrian@mxlinux.org>
2015-01-16 16:45:08 -0500

    fixed typo

--- mxsnapshot.cpp
+++ mxsnapshot.cpp
@@ -65,7 +65,7 @@ void mxsnapshot::setup()
     // Load settings or use the default value
     snapshot_dir = settings.value("snapshot_dir", "/home/snapshot").toString();
     if (system("mountpoint -q /live/aufs") == 0 ) { // if running from a live environment
-        snapshot_dir.setPath("/live/boot.dev/snapshot");
+        snapshot_dir.setPath("/live/boot-dev/snapshot");
     }
     ui->labelSnapshot->setText(tr("The snapshot will be placed by default in ") + snapshot_dir.absolutePath());
     snapshot_excludes.setFileName(settings.value("snapshot_excludes", "/usr/lib/mx-snapshot/snapshot-exclude.list").toString());
