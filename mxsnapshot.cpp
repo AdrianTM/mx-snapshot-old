24da91b3abcd1fc1455908ccdc0a34b3281dcaa6
AdrianTM <adrian@mxlinux.org>
2015-02-10 11:10:04 -0500

    minor change

--- mxsnapshot.cpp
+++ mxsnapshot.cpp
@@ -446,7 +446,7 @@ void mxsnapshot::setupEnv()
             if (!checkInstalled("live-init-mx")) {
                 runCmd("apt-get install live-init-mx");
                 // fix antiX-init start-up
-                system("update-rc.d antiX-init defaults");
+                system("update-rc.d antiX-init defaults >/dev/null 2>&1");
             }
         }
         // copy files that need to be edited to work_dir
