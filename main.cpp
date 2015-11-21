d1c59415c756f12ae4e808ef386985b4c04a1078
Adrian <adrian@mxlinux.org>
2015-11-21 12:12:27 -0500

    moved icon, rm mx-installed-to-live script

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx/mx-snapshot.png"));
+    a.setWindowIcon(QIcon("/usr/share/pixmaps/mx-snapshot.png"));
 
     QTranslator qtTran;
     qtTran.load(QString("qt_") + QLocale::system().name());
